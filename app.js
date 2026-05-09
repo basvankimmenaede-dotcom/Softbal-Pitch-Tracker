ad423cd46b5abccdab1442c3d324edd8557bbe9d
basvankimmenaede-dotcom <basvankimmenaede@gmail.com>
2026-05-09 12:51:49 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+// OG Pitching Tracker App
+
+const pitchTypeOptions = ["Fastball", "Slowball", "Overig"];
+const resultOptions = ["Ball", "Strike", "Swing", "Foul", "HIT", "Out"];
+
+function init() {
+  console.log("OG Pitching Tracker geladen");
+}
+
+window.onload = init;
